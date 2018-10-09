32de2681cb2779860fc6474c1cb685768da2bd83
roshankumarbhuyan <38334530+roshankumarbhuyan@users.noreply.github.com>
2018-10-09 22:37:48 +0200

    Update imageslice.py

--- imageslice.py
+++ imageslice.py
@@ -2,7 +2,8 @@ import image_slicer as slicer
 import os, sys
 
 basedir = os.getcwd()
-
-for fn in os.listdir(basedir):
+img_dir = os.path.join(basedir, "jpg")
+os.chdir(img_dir)
+for fn in os.listdir(img_dir):
 	print(fn)
 	slicer.slice(fn, 6)
